4961f5706307527eba304e2b173a4f3f421b0636
Ashu <ashu1singh2@rediffmail.com>
2025-01-06 02:20:50 +0530

    removed

--- controllers/healthcheck.controller.ts
+++ controllers/healthcheck.controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 
 export const healthcheck = async (req:Request, res: Response) => {
     try {
-      console.log(req);
       res.status(200).json({
         success: true,
         message: {Status : 'UP'},
