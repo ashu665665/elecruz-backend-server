b142703f384c0c6e89f25d5d0f8fee9e47c92d16
Ashu <ashu1singh2@rediffmail.com>
2025-01-06 02:48:52 +0530

    remove logger

--- build/controllers/healthcheck.controller.js
+++ build/controllers/healthcheck.controller.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.healthcheck = void 0;
 const healthcheck = async (req, res) => {
     try {
-        console.log(req);
         res.status(200).json({
             success: true,
             message: { Status: 'UP' },
